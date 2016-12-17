be278ff2ac35ad00a48ec4419feccff1f9a00d84
transformGit <2630243397@qq.com>
2016-12-17 17:07:10 +0800

    basic layout

---
+++ js/main.js
@@ -0,0 +1,42 @@
+ (function() {
+   var toggle = false;
+   var speed = 200;
+   var $aside = $('#aside'),
+       $main = $('#main'),
+       $header = $('#navbar-header');
+   //侧边栏显示隐藏
+    $('#btnToggle').on('click', function() {
+       if (!toggle) {
+         $aside.animate({ 'left': -240 }, speed)
+         $main.animate({ 'left': 0 }, speed)
+         $header.animate({paddingRight: 0}, speed)
+       }else{
+         $aside.animate({ 'left': 0 }, speed)
+         $main.animate({ 'left': 240 }, speed)
+         $header.animate({paddingRight: 240}, speed)
+       }
+       toggle = !toggle
+    })
+   //手风琴菜单
+   var Accordion = function(element, multiple) {
+     this.element = element || {};
+     this.multiple = multiple || false;
+
+     var links = this.element.find('.link');
+     links.on('click', {
+       element: this.element,
+       multiple: this.multiple
+     }, this.dropdown)
+   }
+   Accordion.prototype.dropdown = function(e) {
+     var $element = e.data.element;
+     var $this = $(this),
+         $next = $this.next();
+     $next.slideToggle();
+     $this.parent().toggleClass('open');
+     if(!e.data.multiple) {
+       $element.find('.submenu').not($next).slideUp().parent().removeClass('open');
+     };
+   }
+   new Accordion($('#accordion'));
+ })(jQuery)
